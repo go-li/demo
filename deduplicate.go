9b31aa86999c89c092b924b181a065a9445caeb9
goli admin <charraster@gmail.com>
2016-06-07 09:56:41 +0200

    Create deduplicate.go

---
+++ deduplicate.go
@@ -0,0 +1,44 @@
+package main
+
+func Deduplicate(compare func(*,*)int, arry []*) (l int) {
+ l = len(arry)
+ for i:=0;i<l;i++{
+   for ((compare(arry[l-1], arry[i])==0) && (l-1 > i)){
+        l--
+	}
+  for j:=i+1;j<l;j++{
+
+   if(compare(arry[j], arry[i])==0){
+        arry[i] = arry[l-1]
+        l--
+     }
+  }
+ }
+  return l
+}
+
+type man struct {
+name string
+id byte
+}
+
+func compare_man_by_id(l *man, r *man) int {
+      return int(l.id) - int(r.id)
+}
+
+
+func main() {
+	var y = []*man{&man{"Bob",0},&man{"Pat",1},&man{"Bob",0},&man{"Rob",2},&man{"Bob",0}}
+
+	y = y[:Deduplicate(compare_man_by_id, y)]
+
+	for n := range y {
+		var x man = *(y[n])
+		print(x.name)
+		print(" ")
+		print(x.id)
+		print("\n")
+	}
+
+
+}
