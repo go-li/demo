fffd67acc9788c351cfee6e88ff32ba2af8e6b16
m <a@example.com>
2016-11-08 16:14:09 +0100

    Pointer passer demo up-date

--- broken/pass_anypointer_return.go
+++ broken/pass_anypointer_return.go
@@ -9,13 +9,31 @@ func main() {
 	var foo int
 	var bar string
 	var baz byte
-	
+
 	var a *int = macro_pass(&foo)
 	var b *string = macro_pass(&bar)
 	var c *byte = macro_pass(&baz)
-	
-	foo = a
-	bar = b
-	baz = c
-	
+
+	foo = *a
+	bar = *b
+	baz = *c
+
+	var x = macro_pass(&foo)
+	var y = macro_pass(&bar)
+	var z = macro_pass(&baz)
+
+	foo = *x
+	bar = *y
+	baz = *z
+
+	var u = *macro_pass(&foo)
+	var v = *macro_pass(&bar)
+	var w = *macro_pass(&baz)
+
+	_ = x
+	_ = y
+	_ = z
+	_ = u
+	_ = v
+	_ = w
 }
