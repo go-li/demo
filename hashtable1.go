cc0e0ecba31ca13295cb98cb6b3892030c77aef9
Your Name <you@example.com>
2018-03-29 21:57:14 +0200

    A very performant hashtable
    Actually an array of 128 slice based hashtables
    
    The hops parameter tweaked to performant value

--- hashtable1.go
+++ hashtable1.go
@@ -1,6 +1,33 @@
 package main
 
-const hops = 255
+import "time"
+
+// BEGIN PRNG
+
+var s [2]uint64
+
+func rotl( x uint64,  k uint) uint64 {
+	return (x << k) | (x >> (64 - k));
+}
+
+func next() uint64 {
+	var s0 = s[0];
+	var s1 = s[1];
+	var result = s0 + s1;
+
+	s1 ^= s0;
+	s[0] = rotl(s0, 55) ^ s1 ^ (s1 << 14); // a, b
+	s[1] = rotl(s1, 36); // c
+
+	return result;
+}
+
+// END PRNG
+
+// BEGIN HTABLE
+
+
+const hops = 96
 
 func forEachKey(hash func(*,int) uint64, keys [], do func(*, int)) {
 
@@ -29,37 +56,10 @@ func forEachKey(hash func(*,int) uint64, keys [], do func(*, int)) {
 
 }
 
-func hashfun(key *[8]byte, size int) (o uint64) {
-	o |= uint64(key[0])
-	o |= uint64(key[1]) << 8 
-	o |= uint64(key[2]) << 16 
-	o |= uint64(key[3]) << 24
-	o |= uint64(key[4]) << 32
-	o |= uint64(key[5]) << 40
-	o |= uint64(key[6]) << 48
-	o |= uint64(key[7]) << 56
-
-	if size == -1 {
-		return o
-	}
-
-	return o % uint64(size)
-
-}
-
-
-
-func hashfn(key *int, size int) (o uint64) {
-	switch (size) {
-	case -1: return uint64(*key)
-	default: return (uint64(*key) * 0x61C8864680B583EB) % uint64(size)
-	}
-}
-
 
 type selector int
 
-func (f selector) Remove(table []) selector {
+func (f selector) remove(table []) selector {
 
 	if f == -1 {
 		return -1
@@ -247,7 +247,9 @@ func (f inserter) into(hash func(*, int) uint64, key *, table *[]) {
 
 	if hkey == hloc {
 
+		if f != nil {
 		f(int(slot),-4)
+		}
 
 		return
 	}
@@ -446,104 +448,16 @@ func insert(value *, values *[]) inserter {
 
 }
 
-type keyval struct {
-	key int
-	val int
-}
-
-func kvhfunc(key *keyval, modulo int) (o uint64) {
-
-	return hashfn(&(key.key), modulo)
-/*
-	var x = uint64((*key).key)
-
-	if modulo == -1 {
-		return x
-	}
-
-	o = (
-		((((x >> +0) & 1) - 1) & 0x34b1abcc664e7d08) +
-		((((x >> +1) & 1) - 1) & 0x8984b493db9046ce) +
-		((((x >> +2) & 1) - 1) & 0x9257b296b7128aa1) +
-		((((x >> +3) & 1) - 1) & 0x2541b069313e8711) +
-		((((x >> +4) & 1) - 1) & 0x7e124a220e93b2a9) +
-		((((x >> +5) & 1) - 1) & 0xb3e2573e99a87d28) +
-		((((x >> +6) & 1) - 1) & 0x52d5d851cc301368) +
-		((((x >> +7) & 1) - 1) & 0x4fc24b628be15ef1) +
-		((((x >> +8) & 1) - 1) & 0x98462271312ae033) +
-		((((x >> +9) & 1) - 1) & 0x47d7121596e4a8a7) +
-		((((x >> 10) & 1) - 1) & 0xf96889941a9427ef) +
-		((((x >> 11) & 1) - 1) & 0xd1ae04ebc278f81e) +
-		((((x >> 12) & 1) - 1) & 0x5f6392a366627e89) +
-		((((x >> 13) & 1) - 1) & 0x3e8aae727dea34f5) +
-		((((x >> 14) & 1) - 1) & 0xc77d4169f0959d11) +
-		((((x >> 15) & 1) - 1) & 0x9972d93c1ac5ee97) +
-		((((x >> 16) & 1) - 1) & 0x502267ac8dcec52d) +
-		((((x >> 17) & 1) - 1) & 0xb8216acbbba21c84) +
-		((((x >> 18) & 1) - 1) & 0x4560ec8a83409f86) +
-		((((x >> 19) & 1) - 1) & 0x3783a6fac074e237) +
-		((((x >> 20) & 1) - 1) & 0xf23928a936e9a255) +
-		((((x >> 21) & 1) - 1) & 0x6971e1827421f5b8) +
-		((((x >> 22) & 1) - 1) & 0x378cf244e4525bbd) +
-		((((x >> 23) & 1) - 1) & 0x270334c7b5246eaf) +
-		((((x >> 24) & 1) - 1) & 0xdb00f461834296f9) +
-		((((x >> 25) & 1) - 1) & 0x865b916f3af6785c) +
-		((((x >> 26) & 1) - 1) & 0x0ef31c7299afae18) +
-		((((x >> 27) & 1) - 1) & 0x275cccc6bcfbe655) +
-		((((x >> 28) & 1) - 1) & 0xb679ed17b169952a) +
-		((((x >> 29) & 1) - 1) & 0x1d9735290203b7bd) +
-		((((x >> 30) & 1) - 1) & 0xa28fa59ab03336a0) +
-		((((x >> 31) & 1) - 1) & 0x223f894ab56a927d) +
-		((((x >> 32) & 1) - 1) & 0x7525cef820c1d003) +
-		((((x >> 33) & 1) - 1) & 0x7a8a4b20547a16e1) +
-		((((x >> 34) & 1) - 1) & 0x60062881f00946f2) +
-		((((x >> 35) & 1) - 1) & 0x2900e59c29960321) +
-		((((x >> 36) & 1) - 1) & 0xaab883262559d99b) +
-		((((x >> 37) & 1) - 1) & 0x143842c43dd6d612) +
-		((((x >> 38) & 1) - 1) & 0x3bfc780b1279edb7) +
-		((((x >> 39) & 1) - 1) & 0x35894032901336ca) +
-		((((x >> 40) & 1) - 1) & 0xda5c1457870482fe) +
-		((((x >> 41) & 1) - 1) & 0x3eb30aa435cd58b8) +
-		((((x >> 42) & 1) - 1) & 0x2483a66644cd97b6) +
-		((((x >> 43) & 1) - 1) & 0x67f721ef981c768f) +
-		((((x >> 44) & 1) - 1) & 0x574e495f1519c828) +
-		((((x >> 45) & 1) - 1) & 0x88effc0102de79c1) +
-		((((x >> 46) & 1) - 1) & 0xf348c37ad4130787) +
-		((((x >> 47) & 1) - 1) & 0x6ff5a240eb06d7ce) +
-		((((x >> 48) & 1) - 1) & 0xf4ea2abcf4a01533) +
-		((((x >> 49) & 1) - 1) & 0x91ca718ac8d4dbfe) +
-		((((x >> 50) & 1) - 1) & 0xdeb03e2f8404e0c7) +
-		((((x >> 51) & 1) - 1) & 0x733a8f26baec9dcb) +
-		((((x >> 52) & 1) - 1) & 0x58c69a5d9c02795a) +
-		((((x >> 53) & 1) - 1) & 0xfb69b32a68334a13) +
-		((((x >> 54) & 1) - 1) & 0x9e9137026c76a891) +
-		((((x >> 55) & 1) - 1) & 0x3c990e2b9c74bc78) +
-		((((x >> 56) & 1) - 1) & 0x59939482968310b0) +
-		((((x >> 57) & 1) - 1) & 0xb8172843814db2fb) +
-		((((x >> 58) & 1) - 1) & 0x0d403ab99a734832) +
-		((((x >> 59) & 1) - 1) & 0x8cf7b79d0d9d45c8) +
-		((((x >> 60) & 1) - 1) & 0x87ef5765487e2285) +
-		((((x >> 61) & 1) - 1) & 0xd220b2fa846a281d) +
-		((((x >> 62) & 1) - 1) & 0x0450a5a5d76cb644) +
-		((((x >> 63) & 1) - 1) & 0x536dd65bd43624d8) +
-	0)
-
-
-
-	return o % uint64(modulo)
-*/
-}
 
 //// VACUUM HASHTABLE //////////////////////////////////////////////////////////
 
 // vacuum a hashtable
-// this procedure is not finished, it fails on a hashtable with a deleted items
 
 
 
 type vacuumer func(int,int)
 
-func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (displaced int) {
+func (f vacuumer) table(hash func(*, int) uint64, table [], accuracy byte) (displaced int) {
 
 	if (accuracy < 1) || (accuracy > 8) {
 		panic("vacuum accuracy should be small: 1,2,3,4 etc")
@@ -612,7 +526,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 */
 		if vacancy == hvoid {
 
-		f(-1, nexslot)
+		if f != nil {
+			f(-1, nexslot)
+		}
 		*void = *posptr
 
 		var src *;
@@ -621,7 +537,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 			displaced++
 		posptr = &table[mynexslot]
 		src = &table[buf[0][0]]
-		f(mynexslot, buf[0][0])
+		if f != nil {
+			f(mynexslot, buf[0][0])
+		}
 		*posptr = *src
 /*
 		print(mynexslot);print(" <- ");println(buf[0][0])
@@ -644,8 +562,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 */
 		}
 
-
-		f(buf[0][0],-1)
+		if f != nil {
+			f(buf[0][0],-1)
+		}
 		src = &table[buf[0][0]]
 		*src = *void
 
@@ -678,7 +597,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 /*
 			print("put away ");println(nexslot)
 */
-			f(-1, nexslot)
+			if f != nil {
+				f(-1, nexslot)
+			}
 			ptr = &table[nexslot]
 			*void = *ptr
 
@@ -695,7 +616,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 				panic("not a loop")
 			}
 
-			f(target, nextarget)
+			if f != nil {
+				f(target, nextarget)
+			}
 			dst = &table[target]
 			ptr = &table[nextarget]
 			*dst = *ptr
@@ -720,7 +643,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 //			delete(mapa,target)
 			mapb[target&mapbsize] = mapblank
 
-			f(target, -1)
+			if f != nil {
+				f(target, -1)
+			}
 			dst = &table[target]
 			*dst = *void
 
@@ -775,7 +700,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 */
 		if vacancy == hvoid {
 
-		f(-1, nexslot)
+		if f != nil {
+			f(-1, nexslot)
+		}
 		*void = *posptr
 
 		var src *;
@@ -784,7 +711,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 			displaced++
 		posptr = &table[mynexslot]
 		src = &table[buf[i][0]]
-		f(mynexslot, buf[i][0])
+		if f != nil {
+			f(mynexslot, buf[i][0])
+		}
 		*posptr = *src
 /*
 		print(mynexslot);print(" <- ");println(buf[i][0])
@@ -807,8 +736,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 */
 		}
 
-
-		f(buf[i][0],-1)
+		if f != nil {
+			f(buf[i][0],-1)
+		}
 		src = &table[buf[i][0]]
 		*src = *void
 
@@ -837,7 +767,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 /*
 			print("put away ");println(nexslot)
 */
-			f(-1, nexslot)
+			if f != nil {
+				f(-1, nexslot)
+			}
 			ptr = &table[nexslot]
 			*void = *ptr
 
@@ -854,7 +786,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 				panic("not a loop")
 			}
 
-			f(target, nextarget)
+			if f != nil {
+				f(target, nextarget)
+			}
 			dst = &table[target]
 			ptr = &table[nextarget]
 			*dst = *ptr
@@ -879,7 +813,9 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 //			delete(mapa,target)
 			mapb[target&mapbsize] = mapblank
 
-			f(target, -1)
+			if f != nil {
+				f(target, -1)
+			}
 			dst = &table[target]
 			*dst = *void
 
@@ -899,9 +835,7 @@ func (f vacuumer) Table(hash func(*, int) uint64, table [], accuracy byte) (disp
 func vacuum(values []) vacuumer {
 
 	if len(values) == 0 {
-		return vacuumer( func(dst int, src int) {
-
-		})
+		return nil
 	}
 
 	var putaway []
@@ -958,7 +892,7 @@ type deletor func(int,int)
 func delet(values []) deletor {
 
 	if len(values) == 0 {
-		return func(int,int){}
+		return nil
 	}
 
 	return func(i int, j int) {
@@ -1030,7 +964,7 @@ func (d deletor) from(hash func(*, int) uint64, key *, table []) {
 	}
 
 
-	for i := slot + 1 ; (int(i) < len(table)) && (i < slot + hops); i++ {
+	for i := slot + 1 ; (int(i) < len(table)) && (i < slot + uint64(hops)); i++ {
 
 		var item *;
 
@@ -1056,7 +990,9 @@ func (d deletor) from(hash func(*, int) uint64, key *, table []) {
 //		print(itemloc)
 //		println(" FIXING BURST")
 
-		d(int(slot), int(i))
+		if d != nil {
+			d(int(slot), int(i))
+		}
 
 		*location = *item
 		*item = *void
@@ -1070,280 +1006,97 @@ func (d deletor) from(hash func(*, int) uint64, key *, table []) {
 	}
 }
 
-////////////////////////////////////////////////////////////////////////////////
+// --- 128 slice based hashtables
+
+func (f inserter) into128(hash func(*, int) uint64, key *, table *[128][]) {
+	var k = hash(key, -1)
+
+	f.into(hash, key, &(*table)[k&127])
+}
+
+func (f vacuumer) table128(hash func(*, int) uint64, table *[128][], accuracy byte) (displaced int) {
+	for i := 0; i < 128; i++ {
+		displaced += f.table(hash, (*table)[i], accuracy)
+	}
+	return displaced
+}
+
+func (d deletor) from128(hash func(*, int) uint64, key *, table *[128][]) {
+	var k = hash(key, -1)
+
+	d.from(hash, key, (*table)[k&127])
+}
+
+
+//---
+
+func hash32(ptr *uint32, mod int) uint64 {
+	if mod == -1 {
+		return uint64(*ptr)
+	}
+	var p = uint64(*ptr)
+	var m = uint64(mod)
+	return (uint64(uint32(p * m)) * m) >> 32
+}
+
+func hash16(ptr *uint16, mod int) uint64 {
+	var p = uint64(*ptr)
+	var m = uint64(mod)
+	return (uint64(uint16(p * m)) * m) >> 16
+}
+
+
+func hash16mod(ptr *uint16, mod int) uint64 {
+	return uint64(uint64(*ptr) % uint64(mod))
+}
+
+func hash32mod(ptr *uint32, mod int) uint64 {
+	return uint64(uint64(*ptr) % uint64(mod))
+}
+
+//
 
 func main() {
 
-	var keys [][8]byte
-	var values [][2]uintptr
-	_ = values
+
+	s = [2]uint64{0xbeac0467eba5facd, 0xd86b048b86aa9922}
 
 
-	var test = [2]uintptr{1337,7331}
+	var hashmap [128][]uint32
 
-	insert(&test, &values).into(hashfun, &[8]byte{2,6,7,8,3,2,1,0}, &keys)
-	insert(&test, &values).into(hashfun, &[8]byte{2,6,7,8,3,2,1,0}, &keys)
-	insert(&test, &values).into(hashfun, &[8]byte{2,6,7,8,3,2,1,1}, &keys)
-	insert(&test, &values).into(hashfun, &[8]byte{2,6,7,8,3,2,1,2}, &keys)
-	insert(&test, &values).into(hashfun, &[8]byte{2,6,7,8,3,2,1,3}, &keys)
-	insert(&test, &values).into(hashfun, &[8]byte{2,6,7,8,3,2,1,4}, &keys)
-	insert(&test, &values).into(hashfun, &[8]byte{1,3,8,5,4,9,5,3}, &keys)
+	var begin = time.Now().UnixNano()
 
-	for i := 0; i < 250; i++ {
 
-		var oldsize = len(keys)
 
-		insert(&test, &values).into(hashfun, &[8]byte{1,3,8,5,4,9,5,byte(i)}, &keys)
 
-		if oldsize != len(keys) {
 
-			print("Reallocated to size ");
-			println(len(keys))
-		}
+	for i := 0; i < 1000000; i++ {
+		var k = uint32(next())
+		inserter(nil).into128(hash32, &k, &hashmap)
 	}
 
 
 
-	println("Here.")
+//	vacuumer(nil).table128(hash32, &hashmap, 6)
 
-	forEachKey(hashfun, keys, func(key *[8]byte, i int) {
 
-		print(i)
+	s = [2]uint64{0xbeac0467eba5facd, 0xd86b048b86aa9922}
 
-		print(" ")
-		print(values[i][0])
-		print("  ")
+	for i := 0; i < 1000000; i++ {
+		var k = uint32(next())
+		deletor(nil).from128(hash32, &k, &hashmap)
+	}
+
+
+	var end = time.Now().UnixNano()
+	println(end - begin)
+
+	for i := 0; i < len(hashmap); i++ {
+	forEachKey(hash32, hashmap[i], func(*uint32, int) {
+		panic("Something is in emptied table");
 	})
-	println("")
-	println("Searching.")
-
-	var a *[2]uintptr = slect(hashfun, &[8]byte{1,3,8,5,4,9,5,20}, keys).from(values)
-
-	print((*a)[0])
-	println((*a)[1])
-
-	if nil == slect(hashfun, &[8]byte{0,0,0,0,1,0,0,1}, keys).from(values) {
-		println("not found")
-	}
-
-	///////////
-
-	println("///////")
-
-	var keyz []int
-	var vals []int
-
-	for i := 5; i < 1000; i++ {
-
-	var lenkeys = len(keyz)
-	_ = lenkeys
-
-	var j int = i
-
-	insert(&j, &vals).into(hashfn, &j, &keyz)
-
-	if lenkeys != len(keyz) {
-
-
-	print(i-4)
-	print("|")
-	print(len(keyz))
-	print("|")
-
-
-
-//	for j :=range keyz {
-//		print(keyz[j])
-//		print(" ")
-//	}
-
-		println("--")
-	}
-	}
-
-	println(vacuum(vals).Table(hashfn, keyz, 4))
-
-	delet(vals).from(hashfn, &[]int{10}[0], keyz)
-
-	for i := 5; i < 15; i++ {
-		var v *int = slect(hashfn, &i, keyz).from(vals)
-		print(i)
-		print(" ")
-		if v == nil {println("nil");} else {
-		println(*v)
-		}
 	}
 
 
-	///////////
-/*
-	println("///////")
 
-	var gomap = make( map[int]int)
-
-	for i := 2; i < 1000000; i++ {
-
-		gomap[i] = i
-	}
-
-	for j := 500000; j < 1000000; j++ {
-		delete(gomap, j)
-	}
-
-	for j := 500000; j < 1439470; j++ {
-
-		gomap[j] = j
-
-	}
-
-	var total = 0
-
-	for q := 0; q < 10; q++ {
-	for i := 2; i < 1439470; i++ {
-
-
-		var a, ok = gomap[i]
-
-		if !ok {
-			println(i)
-		}
-
-		total += a
-
-	}
-	}
-
-	println(total)
-
-	return
-*/
-	///////////
-
-	println("///////")
-
-	var kval []keyval
-
-	for i := 2; i < 1000000; i++ {
-
-	var lenkeys = len(kval)
-	_ = lenkeys
-
-	insert(nil, &kval).into(kvhfunc, &keyval{i,i}, &kval)
-
-	if lenkeys != len(kval) {
-
-
-	print(i-1)
-	print("|")
-	print(len(kval))
-	print("|")
-	println("--")
-	}
-
-	}
-/*
-	for i := range kval {
-		print("[")
-		print(i)
-		print("][")
-		print(kvhfunc(&kval[i], len(kval)-hops))
-		print("]")
-		println(kval[i].key)
-	}
-*/
-/*
-	slect(kvhfunc, &keyval{key:11}, kval).Remove(kval)
-
-	for i := 2; i < 300; i++ {
-
-
-		var a *keyval = slect(kvhfunc, &keyval{key:i}, kval).from(kval)
-
-		if a == nil {
-		println("- -")
-		continue
-		}
-
-		print(a.key)
-		print(" ")
-		println(a.val)
-	}
-*/
-	// VACUUM
-
-
-//	println(vacuum([]struct{}{}).Table(kvhfunc, kval))
-//	println(vacuum([]struct{}{}).Table(kvhfunc, kval))
-
-/*
-	for i := range kval {
-		print("[")
-		print(i)
-		print("][")
-		print(kvhfunc(&kval[i], len(kval)-hops))
-		print("]")
-		println(kval[i].key)
-	}
-*/
-
-
-	for j := 500000; j < 1000000; j++ {
-
-		delet([]struct{}{}).from(kvhfunc, &keyval{key:j}, kval)
-	}
-
-	println(vacuum([]struct{}{}).Table(kvhfunc, kval, 1))
-/*	{
-		var tmp []keyval
-
-		forEachKey(kvhfunc, kval, func(p *keyval, o int) {
-			insert(nil, &tmp).into(kvhfunc, p, &tmp)
-		})
-		kval = tmp
-		tmp = nil
-	}
-*/
-	for j := 500000; j < 1439470; j++ {
-
-	insert(nil, &kval).into(kvhfunc, &keyval{j,j}, &kval)
-
-	}
-
-	println(vacuum([]struct{}{}).Table(kvhfunc, kval, 4))
-/*
-	{
-		var tmp []keyval
-
-		forEachKey(kvhfunc, kval, func(p *keyval, o int) {
-			insert(nil, &tmp).into(kvhfunc, p, &tmp)
-		})
-		kval = tmp
-		tmp = nil
-	}
-*/
-	var key keyval
-
-	for q := 0; q < 10; q++ {
-	for i := 2; i < 1439470; i++ {
-
-		key.key = i
-
-		var a *keyval = fetch(kvhfunc, &key, kval)
-
-		if a == nil {
-			println(i)
-		}
-
-	}
-	}
-/*
-	println("///")
-
-	////
-	for i := 1; i > 0; i = grow(i) {
-		print(i)
-		print(" ")
-		println(byte((((((((i-1)<<1)+19)&0x1fff)^104)%29))))
-	}
-*/
 }
